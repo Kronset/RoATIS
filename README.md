7cb6a1a2005b499617bd69f0cf92068be37b2fab
Kronset <68067447+Kronset@users.noreply.github.com>
2024-07-30 21:04:31 +0200

    Source

--- README.md
+++ README.md
@@ -1,18 +1,1 @@
 # RoATIS
-![Banner](https://media.discordapp.net/attachments/1119182711584731247/1267850724692459580/image.png?ex=66aa4980&is=66a8f800&hm=128b81068851ec8c3b0aba0b6a6262460dcacebaad2b53036c1725a501f12438&=&format=webp&quality=lossless)
-
-# What's RoATIS?
-RoATIS was created to make life easier for Roblox air traffic controllers! The program is currently in BETA version and provides only basic functions such as generating ATIS based on the provided data.
-
-# Features
-- Make own ATIS message to improve realism on your session!
-- Generated ATIS is roblox-friendly wich mean that roblox will not delete your message!
-
-> [!WARNING]
-> RoATIS is currently in early BETA!
-
-Contact: Discord: @kronset_
-
-Last Updated 30/07/2024
-
-
---
+++ RoATIS/RoATIS.cpp
@@ -0,0 +1,124 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include <windows.h>
+#include <ctime>
+#include <cstdlib>
+
+// TODO: airport info save system
+using namespace std;
+
+string a, direct, vis, dep, arr, inf;
+int b, c;
+bool isKnown, isDefault;
+
+int main() {
+    int temp1;
+    string temp2;
+
+    // Banner
+    cout << "  _____            _______ _____  _____ \n";
+    cout << " |  __ \\        /\\|__   __|_   _|/ ____|\n";
+    cout << " | |__) |___   /  \\  | |    | | | (___ \n";
+    cout << " |  _  // _ \\ / /\\ \\ | |    | |  \\___ \\ \n";
+    cout << " | | \\ \\ (_) / ____ \\| |   _| |_ ____) |\n";
+    cout << " |_|  \\_\\___/_/    \\_\\_|  |_____|_____/ \n" << endl;
+    cout << ">> Welcome in RoATIS!" << endl << ">> Application Version 0.0.1!" << endl << ">> Made by kronset_" << endl << ">> Follow me on github https://github.com/Kronset!\n" << endl;
+
+    // Input airport ICAO code
+    cout << "What's your airport ICAO code?" << endl << ">> ";
+    cin >> a;
+
+    // Check if ICAO is known
+    // add if todo done a == "JTPH" || a == "JSLL" || a == "JC04"
+    if (!a.empty()) {
+        cout << "Your ICAO is: " << a << endl << endl;
+        // change to true when todo done
+        isKnown = false;
+    }
+    else if (!a.empty()) {
+        cout << "Your ICAO is: " << a << endl << endl;
+        isKnown = false;
+    }
+    else {
+        cout << "Invalid Input!" << endl;
+        return 0;
+    }
+
+    // Set wind speed
+    cout << "Wind (in kts)" << endl << ">> ";
+    cin >> b;
+    if (b > 0) {
+        cout << "Wind set to: " << b << " kts\n" << endl;
+    }
+    else {
+        cout << "Invalid Input!" << endl;
+        return 0;
+    }
+
+    // Set wind direction
+    cout << "Wind direction (in degrees)" << endl << ">> ";
+    cin >> c;
+    if (c >= 0 && c <= 360) {
+        if (c >= 1 && c <= 89) direct = "NW";
+        else if (c >= 91 && c <= 179) direct = "SE";
+        else if (c >= 181 && c <= 269) direct = "SW";
+        else if (c >= 271 && c <= 359) direct = "NW";
+        else if (c == 0) direct = "N";
+        else if (c == 90) direct = "E";
+        else if (c == 180) direct = "S";
+        else if (c == 270) direct = "W";
+
+        cout << "Wind direction set to hdg: " << c << " " << direct << endl << endl;
+    }
+    else {
+        cout << "Error! (0x0002)" << endl;
+        return 0;
+    }
+
+    // Set visibility
+    cout << "Visibility (in km)" << endl << ">> ";
+    cin >> temp1;
+    if (temp1 >= 10) {
+        vis = "10 KM OR MORE";
+        cout << "Visibility set to: " << vis << endl << endl;
+    }
+    else if (temp1 < 10 && temp1 > 0) {
+        vis = temp1;
+    }
+    else {
+        cout << "Error! (0x0003)" << endl;
+        return 0;
+    }
+
+    // Set departure and arrival runways
+    if (isKnown) {
+        cout << "We've detected your ICAO in our database! So you don't have to set up runways!" << endl << "Do you want to use default dep / arr config for " << a << "? (yes/no)" << endl;
+        cin >> temp2;
+        if (!temp2.empty() && temp2 == "yes") {
+            isDefault = true;
+        }
+        else if (!temp2.empty() && temp2 == "no") {
+            isDefault = false;
+        }
+        else {
+            cout << "Error! (0x0004)" << endl;
+            return 0;
+        }
+    }
+    //change before 1.0.1
+    else {
+        cout << "Your airport isn't in our database, you have to insert your runways manually"<< endl;
+    }
+    //Runway set
+    cout << "Insert runway for departures" << endl << ">> ";
+    cin >> dep;
+    cout << "Insert runway for arrivals" << endl << ">> ";
+    cin >> arr;
+    //Information end...
+    cout << "Name wanted information (A-Z)" << endl << ">> ";
+    cin >> inf;
+    cout << "Generated ATIS: " << endl;
+    cout << "THIS IS " << a << " INFROMATION " << inf << " WIND " << c << " KTS " << "HEADING " << direct << " VISIBILITY " << vis << " KM " << "EXPECT APPROACH TO RUNWAY " << arr << " ON INITIAL CONTACT ADVICE " << inf << " ON BOARD " << a << " OUT ";
+    return 0;
+}
